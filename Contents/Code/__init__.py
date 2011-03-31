fc7c6a8978f18c8c5eaeca243053a304ee8bbe02
sander1 <sanderspies+plexapp@gmail.com>
2011-03-31 02:51:59 +0200

    Retrieve thumbnails in a GetThumb function, check if the thumb urls contains 'dynaboss'

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,9 +1,6 @@
-# PMS plugin framework
 import re
 
 ####################################################################################################
-
-VIDEO_PREFIX = "/video/nickjr"
 
 NAMESPACES = {'media':'http://search.yahoo.com/mrss/', 'mediaad':'http://blip.tv/mediaad'}
 
@@ -12,33 +9,41 @@ NICK_SHOWS_LIST      = "http://www.nickjr.com/common/data/kids/get-kids-config-d
 RSS_FEED             = "http://www.nickjr.com/dynamo/video/data/mrssGen.jhtml?type=network&loc=default&hub=kids&mode=playlist&dartSite=nickjr.playtime.nol&mgid=mgid:cms:item:nickjr.com:%s&demo=null&block=true"
 
 NAME = L('Title')
-ART  = 'art-default.png'
+ART  = 'art-default.jpg'
 ICON = 'icon-default.png'
 
 ####################################################################################################
-
 def Start():
-    Plugin.AddPrefixHandler(VIDEO_PREFIX, MainMenu, L('VideoTitle'), ICON, ART)
+    Plugin.AddPrefixHandler('/video/nickjr', MainMenu, NAME, ICON, ART)
     Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
     Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
     MediaContainer.art = R(ART)
     MediaContainer.title1 = NAME
+    MediaContainer.viewGroup = 'List'
     DirectoryItem.thumb = R(ICON)
+    HTTP.CacheTime = CACHE_1HOUR
 
 ####################################################################################################
 def MainMenu():
-    dir = MediaContainer(mediaType='video', viewGroup='List')
+    dir = MediaContainer(mediaType='video')
     content = JSON.ObjectFromURL(NICK_SHOWS_LIST)
     for item in content['config']['promos'][0]['items']:
         title = item['title'].replace('&amp;', '&')
-        image = NICK_ROOT + item['thumbnail']
+
+        if item['thumbnail'].endswith('.jpg'):
+          image = NICK_ROOT + item['thumbnail']
+        elif item['promoImage'].endswith('.jpg'):
+          image = NICK_ROOT + item['promoImage']
+        else:
+          image = None
+
         link = NICK_ROOT + item['link']
-        dir.Append(Function(DirectoryItem(ShowList, title, thumb=image), image = image, pageUrl = link))
+        dir.Append(Function(DirectoryItem(ShowList, title, thumb=Function(GetThumb, url=image)), image = image, pageUrl = link))
     return dir
 
 ####################################################################################################
 def ShowList(sender, image, pageUrl):
-    dir = MediaContainer(title2=sender.itemTitle, viewGroup='List')
+    dir = MediaContainer(title2=sender.itemTitle)
     clipdict = {}
     clipdict['false'] = []
     clipdict['true'] = []
@@ -46,11 +51,15 @@ def ShowList(sender, image, pageUrl):
     m = re.compile('KIDS.add."cmsId", ".+?".;').findall(showcontent)
     cmsid = m[0].split('"')[3]
     url = RSS_FEED % (cmsid)
-    feedcontent = XML.ElementFromURL(url) #.xpath("//item")
-    for item in feedcontent.xpath("//item"): #[0].xpath("..//media:category[@label='full']", namespaces=NAMESPACES):
+    feedcontent = XML.ElementFromURL(url)
+    for item in feedcontent.xpath("//item"):
         title = item.xpath(".//media:title", namespaces=NAMESPACES)[0].text
         link = item.xpath('.//media:player', namespaces=NAMESPACES)[0].get('url')
+
         thumb = item.xpath('.//media:thumbnail', namespaces=NAMESPACES)[0].get('url')
+        if thumb.find('dynaboss') == -1:
+            thumb = thumb.replace("assets", "dynaboss/assets")
+
         duration = int(item.xpath('.//media:content', namespaces=NAMESPACES)[0].get('duration').replace(':', '')) * 1000   ##ADDED replace(':', '') FOR PROTECTION AGAINST A FEW VIDEOS THAT HAVE "MIN:SEC" RATHER THAN JUST "SEC" 
         summary = item.xpath('.//media:description', namespaces=NAMESPACES)[0].text
         if item[0].xpath("..//media:category[@label='full']", namespaces=NAMESPACES):
@@ -58,14 +67,26 @@ def ShowList(sender, image, pageUrl):
         elif item[0].xpath("..//media:category[@label='Playtime Clip']", namespaces=NAMESPACES):
             clipdict['true'].append((title, thumb, summary, link, duration))
     if clipdict['false']:
-        dir.Append(Function(DirectoryItem(VideoList, title="Full Episodes", thumb=image), videolist=clipdict['false']))
+        dir.Append(Function(DirectoryItem(VideoList, title="Full Episodes", thumb=Function(GetThumb, url=image)), videolist=clipdict['false']))
     if clipdict['true']:
-        dir.Append(Function(DirectoryItem(VideoList, title="Clips", thumb=image), videolist=clipdict['true']))
+        dir.Append(Function(DirectoryItem(VideoList, title="Clips", thumb=Function(GetThumb, url=image)), videolist=clipdict['true']))
     return dir
+
 ####################################################################################################
 def VideoList(sender, videolist):
     dir = MediaContainer(title2=sender.itemTitle, viewGroup='InfoList')
     for vid in videolist:
         Log(vid[0])
-        dir.Append(WebVideoItem(url=vid[3], title=vid[0], thumb=vid[1], summary=vid[2], duration=vid[4]))
+        dir.Append(WebVideoItem(url=vid[3], title=vid[0], thumb=Function(GetThumb, url=vid[1]), summary=vid[2], duration=vid[4]))
     return dir
+
+####################################################################################################
+def GetThumb(url):
+    if url:
+        try:
+            image = HTTP.Request(url, cacheTime=CACHE_1WEEK).content
+            return DataObject(image, 'image/jpeg')
+        except:
+            pass
+
+    return Redirect(R(ICON))

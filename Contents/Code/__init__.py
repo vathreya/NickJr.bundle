92102bd4412220eb3f738df84e2b921e467a5e0d
sander1 <sander@plexapp.com>
2014-09-14 23:35:54 +0200

    Add handler/routes; use hosted resources; Update URLPatterns regex.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,49 +1,46 @@
-NAMESPACES = {'media':'http://search.yahoo.com/mrss/'}
-
 NICK_ROOT = 'http://www.nickjr.com'
-NICK_SHOWS_LIST = 'http://www.nickjr.com/common/data/kids/get-kids-config-data.jhtml?fsd=/dynaboss&urlAlias=kids-video-landing&af=false'
+NICK_SHOWS_LIST = 'http://www.nickjr.com/common/data/kids/get-kids-config-data.jhtml?urlAlias=kids-video-landing'
 RSS_FEED = 'http://www.nickjr.com/dynamo/video/data/mrssGen.jhtml?type=network&loc=default&hub=kids&mode=playlist&dartSite=nickjr.playtime.nol&mgid=mgid:cms:item:nickjr.com:%s&demo=null&block=true'
 
-NAME = 'Nick Jr.'
-ART = 'art-default.jpg'
-ICON = 'icon-default.png'
+NAMESPACES = {'media':'http://search.yahoo.com/mrss/'}
 
 RE_CMSID = Regex('KIDS\.add\("cmsId", "(\d+)"\)')
 
 ####################################################################################################
 def Start():
 
-	Plugin.AddPrefixHandler('/video/nickjr', MainMenu, NAME, ICON, ART)
-	Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
-
-	ObjectContainer.title1 = NAME
-	ObjectContainer.view_group = 'InfoList'
-	ObjectContainer.art = R(ART)
-
+	ObjectContainer.title1 = 'Nick Jr.'
 	HTTP.CacheTime = CACHE_1HOUR
-	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:15.0) Gecko/20100101 Firefox/15.0.1'
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.94 Safari/537.36'
 
 ####################################################################################################
+@handler('/video/nickjr', 'Nick Jr.')
 def MainMenu():
 
 	oc = ObjectContainer()
 	content = JSON.ObjectFromURL(NICK_SHOWS_LIST)
 
 	for item in content['config']['promos'][0]['items']:
+
 		title = item['title'].replace('&amp;', '&')
 
-		if title == 'Featured Nick Jr. Videos':
+		if title.lower() in ('featured nick jr. videos'):
 			continue
 
 		thumb = NICK_ROOT + item['thumbnail']
 		url = NICK_ROOT + item['link']
 
-		oc.add(DirectoryObject(key=Callback(ShowList, title=title, thumb=thumb, url=url), title=title, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=R(ICON))))
+		oc.add(DirectoryObject(
+			key = Callback(ShowList, title=title, url=url),
+			title = title,
+			thumb = Resource.ContentsOfURLWithFallback(url=thumb)
+		))
 
 	return oc
 
 ####################################################################################################
-def ShowList(title, thumb, url):
+@route('/video/nickjr/show')
+def ShowList(title, url):
 
 	oc = ObjectContainer(title2=title)
 
@@ -53,17 +50,16 @@ def ShowList(title, thumb, url):
 
 	for item in feed.xpath('//item'):
 
-		title = item.xpath('.//media:title', namespaces=NAMESPACES)[0].text
-		summary = item.xpath('.//media:description', namespaces=NAMESPACES)[0].text
-		url = item.xpath('.//media:player', namespaces=NAMESPACES)[0].get('url')
+		url = item.xpath('.//media:player/@url', namespaces=NAMESPACES)[0]
+		title = item.xpath('.//media:title/text()', namespaces=NAMESPACES)[0]
+		summary = item.xpath('.//media:description/text()', namespaces=NAMESPACES)[0]
+		thumb = item.xpath('.//media:thumbnail/@url', namespaces=NAMESPACES)[0]
 
-		thumb = item.xpath('.//media:thumbnail', namespaces=NAMESPACES)[0].get('url')
-
-		if 'dynaboss' not in thumb:
-			thumb = thumb.replace('assets', 'dynaboss/assets')
+		if '/dynaboss/' in thumb:
+			thumb = thumb.replace('/dynaboss/', '/')
 
 		try:
-			duration = int(item.xpath('.//media:content', namespaces=NAMESPACES)[0].get('duration')) * 1000
+			duration = int(item.xpath('.//media:content/@duration', namespaces=NAMESPACES)[0]) * 1000
 		except:
 			duration = None
 
@@ -72,7 +68,7 @@ def ShowList(title, thumb, url):
 			title = title,
 			summary = summary,
 			duration = duration,
-			thumb = Resource.ContentsOfURLWithFallback(url=thumb, fallback=R(ICON))
+			thumb = Resource.ContentsOfURLWithFallback(url=thumb)
 		))
 
 	return oc
